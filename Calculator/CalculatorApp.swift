7300d590ceacd1f325999e199c78b581f096e251
Kevin Liu <k36liu@uwaterloo.ca>
2022-12-27 17:48:50 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Kevin Liu on 2022-12-27.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
